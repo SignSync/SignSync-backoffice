9e87c4a51e73d6ded6310824460a265123d7e6a3
FYSPA <suarezfloresfy@ag.cbtis225.edu.mx>
2024-11-29 01:56:45 -0600

    Metricas

--- src/app/components/metricas/contratos/contratos.component.ts
+++ src/app/components/metricas/contratos/contratos.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MetricasService } from '../metricas.service';
+
+
+import {
+  Chart,
+  registerables // Importa todos los componentes necesarios
+} from 'chart.js';
 
 @Component({
   selector: 'app-contratos',
@@ -8,5 +15,99 @@ import { Component } from '@angular/core';
   styleUrl: './contratos.component.css'
 })
 export class ContratosComponent {
+  @ViewChild('sexoChartCanvas', { static: true }) sexoChartCanvas!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('registroChartCanvas', { static: true }) registroChartCanvas!: ElementRef<HTMLCanvasElement>;
+  distribucionSexo: { nombre: string; total_paquetes: number }[] = [];
+  distribucionRegistros: { nombre: string; total_documentos: number }[] = [];
+  constructor(private serv:MetricasService){
+    Chart.register(...registerables);
+  }
+
+  ngOnInit(): void {
+    this.fetchDistribucionSexo();
+    this.fetchRegistrosDiarios();
+  }
+
+  fetchDistribucionSexo():void{
+    this.serv.getData('metrics/paquetes-contratos').subscribe({
+      next: (data: any) => {
+        console.log('Datos recibidos:', data);
+        this.distribucionSexo = data.metricas || [];
+        this.createSexoChart();
+      },
+      error: (error) => {
+
+        console.error('Error al obtener las tablas:', error);
+      },
+      complete: () => {
+        console.log('Solicitud completada');
+      }
+    });
+  }
+  fetchRegistrosDiarios():void{
+    this.serv.getData('metrics/documentos-contratos').subscribe({
+      next: (data: any) => {
+        console.log('Datos recibidos:', data);
+        this.distribucionRegistros = data.metricas || [];
+        this.createChartRegistros();
+      },
+      error: (error) => {
+
+        console.error('Error al obtener las tablas:', error);
+      },
+      complete: () => {
+        console.log('Solicitud completada');
+      }
+    });
+  }
+
+  createChartRegistros(): void{
+    const ctx = this.registroChartCanvas.nativeElement.getContext('2d');
+    if (ctx) {
+      new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: this.distribucionRegistros.map(d => d.nombre),
+          datasets: [{
+            label: 'Registros',
+            data: this.distribucionRegistros.map(d => d.total_documentos),
+            backgroundColor: [
+              '#FF99A5', '#FFC966', '#FFEE99', '#6FD7E3', '#BFA1E8',
+              '#FFAA75', '#94E8A6', '#6EC7FF', '#BCAAA4', '#FF9DAA',
+              '#E6E06C', '#D69CF2', '#A8B7F3', '#FFB5D1', '#FFDB7F',
+              '#B8E07C', '#6FD3E6', '#C4DB6F', '#F3FCE6', '#A4D8FF',
+              '#FFAB91', '#C4D933', '#6CCFFF', '#8ED6A7', '#DA82D4'
+            ]
+          }]
+        }
+      });
+    }
+
+  }
+
+  createSexoChart(): void {
+    const ctx = this.sexoChartCanvas.nativeElement.getContext('2d');
+    if (ctx) {
+      new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: this.distribucionSexo.map(d => d.nombre),
+          datasets: [
+            {
+              data: this.distribucionSexo.map(d => d.total_paquetes),
+              backgroundColor: [
+                '#FF99A5', '#FFC966', '#FFEE99', '#6FD7E3', '#BFA1E8',
+                '#FFAA75', '#94E8A6', '#6EC7FF', '#BCAAA4', '#FF9DAA',
+                '#E6E06C', '#D69CF2', '#A8B7F3', '#FFB5D1', '#FFDB7F',
+                '#B8E07C', '#6FD3E6', '#C4DB6F', '#F3FCE6', '#A4D8FF',
+                '#FFAB91', '#C4D933', '#6CCFFF', '#8ED6A7', '#DA82D4'
+              ]
+            }
+          ]
+        }
+      });
+    }
+  }
+
 
 }
--- src/app/components/metricas/usuarios/usuarios.component.ts
+++ src/app/components/metricas/usuarios/usuarios.component.ts
@@ -13,6 +13,8 @@ import {
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
+
+
 export class UsuariosComponent implements OnInit {
   @ViewChild('sexoChartCanvas', { static: true }) sexoChartCanvas!: ElementRef<HTMLCanvasElement>;
   @ViewChild('registroChartCanvas', { static: true }) registroChartCanvas!: ElementRef<HTMLCanvasElement>;
@@ -64,13 +66,19 @@ export class UsuariosComponent implements OnInit {
     const ctx = this.registroChartCanvas.nativeElement.getContext('2d');
     if (ctx) {
       new Chart(ctx, {
-        type: 'bar',
+        type: 'pie',
         data: {
           labels: this.distribucionRegistros.map(d => d.fecha),
           datasets: [{
             label: 'Registros',
             data: this.distribucionRegistros.map(d => d.registros_por_dia),
-            backgroundColor: '#007bff'
+            backgroundColor: [
+              '#FF99A5', '#FFC966', '#FFEE99', '#6FD7E3', '#BFA1E8',
+              '#FFAA75', '#94E8A6', '#6EC7FF', '#BCAAA4', '#FF9DAA',
+              '#E6E06C', '#D69CF2', '#A8B7F3', '#FFB5D1', '#FFDB7F',
+              '#B8E07C', '#6FD3E6', '#C4DB6F', '#F3FCE6', '#A4D8FF',
+              '#FFAB91', '#C4D933', '#6CCFFF', '#8ED6A7', '#DA82D4'
+            ]
           }]
         }
       });
@@ -88,7 +96,13 @@ export class UsuariosComponent implements OnInit {
           datasets: [
             {
               data: this.distribucionSexo.map(d => d.total),
-              backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+              backgroundColor: [
+                '#FF99A5', '#FFC966', '#FFEE99', '#6FD7E3', '#BFA1E8',
+                '#FFAA75', '#94E8A6', '#6EC7FF', '#BCAAA4', '#FF9DAA',
+                '#E6E06C', '#D69CF2', '#A8B7F3', '#FFB5D1', '#FFDB7F',
+                '#B8E07C', '#6FD3E6', '#C4DB6F', '#F3FCE6', '#A4D8FF',
+                '#FFAB91', '#C4D933', '#6CCFFF', '#8ED6A7', '#DA82D4'
+              ]
             }
           ]
         }
